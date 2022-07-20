59a459ff6d6fa20feded1894210fa676e1d4eee3
Daniel Hibbert <26168087+hibbertda@users.noreply.github.com>
2022-07-20 10:28:40 -0400

    tf backend

--- providers.tf
+++ providers.tf
@@ -4,6 +4,12 @@ terraform {
         source = "hashicorp/azurerm"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = "terraform.tfstate"    
+  }
 }
 
 provider "azurerm" {
